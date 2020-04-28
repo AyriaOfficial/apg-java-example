9f83f31f310526cad1329af838e808565f9864b2
SidMorad <SidMorad@users.noreply.github.com>
2020-04-28 16:21:29 +0000

    Another attempt to fix test failure in Github#Actions.

--- src/test/java/com/ayriaplatform/apg/AppTest.java
+++ src/test/java/com/ayriaplatform/apg/AppTest.java
@@ -15,7 +15,7 @@ import junit.framework.TestCase;
 public class AppTest extends TestCase {
 
     private final App app = new App();
-    private static String latestReferenceCode;
+    static String latestReferenceCode;
 
     public void test001Properties() {
         assertNotNull("Referral code should not be null.", app.referralCode());
