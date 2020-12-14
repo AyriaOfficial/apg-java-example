f6db398011f3ae8dc14c0ee28fded9323de05b9a
SidMorad <SidMorad@users.noreply.github.com>
2020-12-14 07:40:22 +0000

    Junit dependency updated 4.7 -> 5.7.0

--- src/test/java/com/ayriaplatform/apg/AppTest.java
+++ src/test/java/com/ayriaplatform/apg/AppTest.java
@@ -1,45 +1,61 @@
 package com.ayriaplatform.apg;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.math.BigDecimal;
 
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.HttpClientErrorException;
 
-import junit.framework.TestCase;
-
 /**
  * Unit test for App
  */
-public class AppTest extends TestCase {
+@TestInstance(Lifecycle.PER_CLASS)
+@TestMethodOrder(OrderAnnotation.class)
+public class AppTest {
 
     private final App app = new App();
     public static String latestReferenceCode;
 
+    @Test
+    @Order(1)
     public void test001Properties() {
-        assertNotNull("Referral code should not be null.", app.referralCode());
-        assertNotNull("APG endpoint should not be null.", app.apgEndpoint());
-        assertNotNull("Payer mobile should not be null.", app.payerMobile());
+        assertNotNull(app.referralCode(), "Referral code should not be null.");
+        assertNotNull(app.apgEndpoint(), "APG endpoint should not be null.");
+        assertNotNull(app.payerMobile(), "Payer mobile should not be null.");
     }
 
+    @Test
+    @Order(2)
     public void test002CreateInvalidCommand() {
         try {
             app.createPayment(new AyriaPaymentV1Command());
             fail("We should get BAD_REQUEST");
         } catch (HttpClientErrorException e) {
-            assertTrue("Error code must be 400", e.getStatusCode().equals(HttpStatus.BAD_REQUEST));
+            assertTrue(e.getStatusCode().equals(HttpStatus.BAD_REQUEST), "Error code must be 400");
         }
     }
 
+    @Test
+    @Order(3)
     public void test003CreateValidCommand() {
         final AyriaPaymentV1Command cmd = new AyriaPaymentV1Command(app.referralCode(), new BigDecimal("1000"), app.payerMobile());
         final ResponseEntity<String> res = app.createPayment(cmd);
-        assertTrue("Status should be 201", res.getStatusCode().equals(HttpStatus.CREATED));
+        assertTrue(res.getStatusCode().equals(HttpStatus.CREATED), "Status should be 201");
         latestReferenceCode = res.getBody();
-        assertTrue("Reference code should not be empty", StringUtils.hasText(latestReferenceCode));
+        assertTrue(StringUtils.hasText(latestReferenceCode), "Reference code should not be empty");
     }
 
+    @Test
+    @Order(4)
     public void test004CreateValidCommandFull() {
         final AyriaPaymentV1Command cmd = new AyriaPaymentV1Command(app.referralCode(), new BigDecimal("1000"), app.payerMobile());
         cmd.setDescription("تراکنش تستی");
@@ -47,14 +63,16 @@ public class AppTest extends TestCase {
         cmd.setPaymentNumber("123456");
         cmd.setExtraData("{\"product\": \"clock\"}");
         final ResponseEntity<String> res = app.createPayment(cmd);
-        assertTrue("Status should be 201", res.getStatusCode().equals(HttpStatus.CREATED));
+        assertTrue(res.getStatusCode().equals(HttpStatus.CREATED), "Status should be 201");
         latestReferenceCode = res.getBody();
-        assertTrue("Reference code should not be empty", StringUtils.hasText(latestReferenceCode));
+        assertTrue(StringUtils.hasText(latestReferenceCode), "Reference code should not be empty");
     }
 
+    @Test
+    @Order(5)
     public void test005GetPayment() {
         final ResponseEntity<AyriaPaymentV1DTO> res = app.getPayment(latestReferenceCode);
-        assertTrue("Status should be 200", res.getStatusCode().equals(HttpStatus.OK));
+        assertTrue(res.getStatusCode().equals(HttpStatus.OK), "Status should be 200");
         final AyriaPaymentV1DTO dto = res.getBody();
         assertEquals(latestReferenceCode, dto.getReferenceCode());
         assertEquals(new BigDecimal("1000"), dto.getAmount());
@@ -67,25 +85,29 @@ public class AppTest extends TestCase {
      // assertEquals("آیریا", dto.getPayeeName());  // Expected value depends to App#referralCode()
         assertFalse(dto.isPaid());
         assertFalse(dto.isCanceled());
-        System.out.println(String.format("Payment created at %s, payment url is: %s", dto.getCreatedDate(), dto.getPaymentUrl()));
+        System.out.println(String.format("Payment created at %s, payment url is: %s", dto.getCreatedDate(), dto.getPaymentUrl().replaceAll("api.ayria.club", "dev.ayria.club")));
     }
 
+    @Test
+    @Order(6)
     public void test006CancelPayment() {
         final AyriaPaymentV1CancelCommand cmd = new AyriaPaymentV1CancelCommand(latestReferenceCode, "قیمت اشتباه");
         final ResponseEntity<AyriaPaymentV1DTO> res = app.cancelPayment(cmd);
-        assertTrue("Status should be 200", res.getStatusCode().equals(HttpStatus.OK));
+        assertTrue(res.getStatusCode().equals(HttpStatus.OK), "Status should be 200");
         final AyriaPaymentV1DTO dto = res.getBody();
         assertTrue(dto.isCanceled());
         assertEquals("قیمت اشتباه", dto.getCancelDescription());
     }
 
+    @Test
+    @Order(7)
     public void test007CancelPaymentInvalid() {
         final AyriaPaymentV1CancelCommand cmd = new AyriaPaymentV1CancelCommand(latestReferenceCode, "قیمت اشتباه");
         try {
             app.cancelPayment(cmd);
             fail("Already canceled payment can't be canceled.");
         } catch (HttpClientErrorException e) {
-            assertTrue("Status should be 400", e.getStatusCode().equals(HttpStatus.BAD_REQUEST));
+            assertTrue(e.getStatusCode().equals(HttpStatus.BAD_REQUEST), "Status should be 400");
         }
     }
 
